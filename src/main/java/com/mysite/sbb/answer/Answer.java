e7822e7d3d4ce0de10e55dd50d4befca921ed2e9
yunalee <yunalee2490@gmail.com>
2025-11-09 22:52:38 +0900

    3-09

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -31,4 +31,6 @@ public class Answer {
     private Question question; 
     @ManyToOne
     private SiteUser author;
+    
+    private LocalDateTime modifyDate;
 }
--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -37,4 +37,6 @@ public class Question {
     private List<Answer> answerList;
     @ManyToOne
     private SiteUser author;
+    
+    private LocalDateTime modifyDate;
 }
